35e4859b26876892f87165d44655dc49feaff60b
Javier Sanchez Panduro <69559745+javiersanpan@users.noreply.github.com>
2021-01-29 14:19:12 -0600

    Included all

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-using namespace std;
+
+#include "notstd.h"
+
+#include "Aeropuerto.h"
+#include "Dia.h"
+#include "Vuelo.h"
 
 int main() {
-  
+
+  cout << "Hello world" << endl;
+
   return 0;
 }
---
+++ notstd.h
@@ -0,0 +1,8 @@
+#ifndef Aeropuerto_H
+#define Aeropuerto_H
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+#endif
