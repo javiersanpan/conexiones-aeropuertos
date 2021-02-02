d15575cbd500107146728271831ddf946d9523c2
Javier Sanchez Panduro <69559745+javiersanpan@users.noreply.github.com>
2021-02-02 13:24:46 -0600

    added new classes

---
+++ Aeropuerto.h
@@ -0,0 +1,14 @@
+//
+// Created by Javier on 02/02/21.
+//
+
+#ifndef CONEXIONES_AEROPUERTOS_AEROPUERTO_H
+#define CONEXIONES_AEROPUERTOS_AEROPUERTO_H
+
+class Aeropuerto {
+private:
+    Horario slots[50];
+public:
+};
+
+#endif //CONEXIONES_AEROPUERTOS_AEROPUERTO_H
---
+++ Horario.h
@@ -0,0 +1,13 @@
+//
+// Created by Javier on 02/02/21.
+//
+
+#ifndef CONEXIONES_AEROPUERTOS_HORARIO_H
+#define CONEXIONES_AEROPUERTOS_HORARIO_H
+
+class Horario {
+private:
+public:
+};
+
+#endif //CONEXIONES_AEROPUERTOS_HORARIO_H
