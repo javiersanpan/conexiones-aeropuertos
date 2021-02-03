9d8eea7bb5b72a0d65c0aa3d9232e07500c4f047
Javier Sanchez Panduro <69559745+javiersanpan@users.noreply.github.com>
2021-02-02 18:30:56 -0600

    Added new headers

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
+// Javier Sánchez Panduro
+// TC1033 Proyecto Integrador
+// Sistema para estar al tanto de la cantidad de aviones actualmente en el aeropuerto
+
+
 #include <iostream>
 using namespace std;
 
+#include "Aeropuerto.h"
+#include "Vuelo.h"
+
 int main() {
-  
-  return 0;
+
+    return 0;
 }
