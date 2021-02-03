6e2a4007f2c6b86f5c5291110a3c21d93354d27d
Javier Sanchez Panduro <69559745+javiersanpan@users.noreply.github.com>
2021-02-02 20:27:47 -0600

    clase vuelo fixes #6

--- Aeropuerto.h
+++ Aeropuerto.h
@@ -1,11 +1,14 @@
 #ifndef CONEXIONES_AEROPUERTOS_AEROPUERTO_H
 #define CONEXIONES_AEROPUERTOS_AEROPUERTO_H
 
+#include "Vuelo.h"
+
 class Aeropuerto {
 private:
     std::string nombre;
     std::string ubicacion;
     int capacidad;
+    Vuelo ;
 public:
 };
 
